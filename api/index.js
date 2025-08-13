5fc281d3b4432d91135ccf3adeadcdf527909dc3
SHADOW123-siva <shivasai.bure@gmail.com>
2025-08-13 20:01:57 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -16,7 +16,4 @@ app.use(express.json());
 app.use('/api', authRouter);    
 app.use('/items', itemsRouter); 
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+module.exports = app;
