b12bb8ae56e554f9cd180b3a2bbb26332035da3b
SHADOW123-siva <shivasai.bure@gmail.com>
2025-08-13 21:06:15 +0530

    create api directory

---
+++ api/db.js
@@ -0,0 +1,14 @@
+const { Pool } = require("pg");
+require("dotenv").config();
+
+const pool = new Pool({
+  host: process.env.PGHOST,
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  database: process.env.PGDATABASE,
+  port: process.env.PGPORT,
+  ssl: false,
+  options: '-c search_path=lostandfound' 
+});
+
+module.exports = pool;
